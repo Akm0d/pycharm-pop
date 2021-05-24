e29cb00c7529494851f520ef5b942a6a9d8f2cc8
Tyler Johnson <tyjohnson@vmware.com>
2021-05-24 02:32:13 -0700

    Removed templated helpers

--- src/main/kotlin/org/jetbrains/plugins/pop_completion/PopBundle.kt
+++ src/main/kotlin/org/jetbrains/plugins/pop_completion/PopBundle.kt
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:src/main/kotlin/com/github/akm0d/pycharmpop/MyBundle.kt
-package com.github.akm0d.pycharmpop
-=======
 package org.jetbrains.plugins.pop_completion
->>>>>>> 22dd244 (Applied project templates):src/main/kotlin/org/jetbrains/plugins/pop_completion/PopBundle.kt
-
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
